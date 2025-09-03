c2f542c4d62613b941e5722f278787c236c06d12
sriiixi <subratoroy@hotmail.com>
2025-09-03 23:46:33 +0530

    C# skeleton code

---
+++ EdgeLiteAPI/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection; 
+
+
+public class RootHandler
+{
+    public static Task Handle(Microsoft.AspNetCore.Http.HttpContext context)
+    {
+        string message = "Hello from .NET 9 Web Service!";
+        context.Response.ContentType = "text/plain";
+        return context.Response.WriteAsync(message);
+    }
+}
+
+public class RoutingConfiguration
+{
+    public static void Configure(IApplicationBuilder app)
+    {
+        app.UseRouting();
+        app.UseEndpoints(ConfigureEndpoints);
+    }
+
+    private static void ConfigureEndpoints(IEndpointRouteBuilder endpoints)
+    {
+        endpoints.MapGet("/", RootHandler.Handle);
+    }
+}
+
+public class WebHostConfiguration
+{
+    public static void Configure(IWebHostBuilder builder)
+    {
+        builder.Configure(RoutingConfiguration.Configure);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Microsoft.AspNetCore.Hosting.IWebHostBuilder hostBuilder = Microsoft.AspNetCore.WebHost.CreateDefaultBuilder(args);
+        WebHostConfiguration.Configure(hostBuilder);
+
+        Microsoft.AspNetCore.Hosting.IWebHost host = hostBuilder.Build();
+        host.Run();
+    }
+}

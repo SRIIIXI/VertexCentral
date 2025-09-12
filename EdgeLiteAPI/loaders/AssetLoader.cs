87fd2e2e42c42ca6373d9eb5d89f091aee598b77
sriiixi <subratoroy@hotmail.com>
2025-09-12 22:59:32 +0530

    Loaders separated

---
+++ EdgeLiteAPI/loaders/AssetLoader.cs
@@ -0,0 +1,48 @@
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+public class AssetLoader : ModelBase
+{
+    AssetLoader() : base("Assets")
+    {
+    }
+    
+    public void Initialize(DataInterface? dataInterface)
+    {
+        selfReference = this;
+        selfReference.connection = dataInterface;
+    }
+
+    public override bool Insert<T>(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool Remove<T>(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool Update<T>(T item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool GetOneOf<T, V>(T item, V value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override bool GetAll<T>(T item)
+    {
+        throw new NotImplementedException();
+    }
+}

faecad5e0b37f28aba7aa34f289eb9800874caf5
sriiixi <subratoroy@hotmail.com>
2025-09-14 18:26:09 +0530

    Login page layout complete

--- axiomcentraui/src/App.js
+++ axiomcentraui/src/App.js
@@ -1,22 +1,25 @@
+
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() 
+{
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/login');
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [navigate]);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ axiomcentraui/src/DashboardSA.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './DashboardSA.css';
+
+function DashboardSA() {
+  return (
+    <div className="DashboardSA">
+      <h1>System Administrator Dashboard</h1>
+      {/* Add widgets, controls, system status here */}
+    </div>
+  );
+}
+
+export default DashboardSA;
+
